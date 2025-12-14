24640a1e2f3af9e97e6fb519f1cc7b38c3beed6b
Thanh Bui <thanhtien.bui.int@shopback.com>
2025-12-14 17:14:33 +0800

    first commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
-from langchain_openai import ChatOpenAI
+# from langchain_openai import ChatOpenAI
 from langchain_ollama import ChatOllama
 
 load_dotenv()
@@ -30,8 +30,8 @@ Musk's political activities, views, and statements have made him a polarizing fi
         input_variables=["information"], template=summary_template
     )
 
-    # llm = ChatOllama(temperature=0, model="gemma3:270m")
-    llm = ChatOpenAI(temperature=0, model="gpt-5")
+    llm = ChatOllama(temperature=0, model="gemma3:270m")
+    # llm = ChatOpenAI(temperature=0, model="gpt-5")
     chain = summary_prompt_template | llm
 
     response = chain.invoke(input={"information": information})
