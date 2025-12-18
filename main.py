92eba6cb46a033349b50aa28aaa693645653da8f
Thanh Bui <thanhtien.bui.int@shopback.com>
2025-12-19 00:11:39 +0800

    first working version

--- main.py
+++ main.py
@@ -2,38 +2,46 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-from langchain.agents import create_agent
+from langchain.agents import AgentExecutor
+from langchain.agents import create_react_agent
+from langchain_core.output_parsers.pydantic import PydanticOutputParser 
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnableLambda
 from langchain_tavily import TavilySearch
 from langchain_ollama import ChatOllama
-
+# from langchain_openai import ChatOpenAI
+from prompt import REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS
 from schemas import AgentResponse
 
-tools = [TavilySearchResults(max_results=5)]
-# llm = ChatOpenAI(model="gpt-4o")
+tools = [TavilySearch(max_results=3)]
+output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
 llm = ChatOllama(temperature=0, model="llama3.1")
+structured_llm = llm.with_structured_output(AgentResponse)
+react_prompt_with_format_instructions = PromptTemplate(
+    input_variables=["input", "agent_scratchpad", "tool_names"],
+    template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
+).partial(format_instructions=output_parser.get_format_instructions())
 
-
-agent = create_agent(
-    model=llm,
+agent = create_react_agent(
+    llm=llm,
     tools=tools,
-    response_format=AgentResponse,
+    prompt=react_prompt_with_format_instructions,
 )
-
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+extract_output = RunnableLambda(lambda x: x["output"])
+parse_ouput = RunnableLambda(lambda x: output_parser.parse(x))
+chain = agent_executor | extract_output | parse_ouput
 
 def main():
-    result = agent.invoke(
-        {
-            "messages": [
-                {
-                    "role": "user",
-                    "content": "search for 3 job summer internship postings for a data-related position in singapore on linkedin and list their details",
-                }
-            ]
+    result = chain.invoke(
+        input = {
+            "input": "search for 3 job summer internship postings for a data-related position in singapore on linkedin and list their details"
         }
     )
     # Access structured response from the agent
-    structured = result.get("structured_response", None)
-    print(structured if structured is not None else result)
+    # structured = result.get("structured_response", None)
+    # print(structured)
+    print(result)
 
 
 if __name__ == "__main__":
