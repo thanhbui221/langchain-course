f72fc0d03666dc98ddded27a7e83308fcc90b80b
thanhbui221 <thanhbui22198@gmail.com>
2025-12-19 20:48:10 +0800

    exp

--- main.py
+++ main.py
@@ -2,45 +2,35 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-from langchain.agents import AgentExecutor
-from langchain.agents import create_react_agent
-from langchain_core.output_parsers.pydantic import PydanticOutputParser 
-from langchain_core.prompts import PromptTemplate
-from langchain_core.runnables import RunnableLambda
+from langchain.agents import create_agent
 from langchain_tavily import TavilySearch
 from langchain_ollama import ChatOllama
-# from langchain_openai import ChatOpenAI
-from prompt import REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS
 from schemas import AgentResponse
+from langchain.messages import HumanMessage, AIMessage, SystemMessage
 
-tools = [TavilySearch(max_results=3)]
-output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
-llm = ChatOllama(temperature=0, model="llama3.1")
-structured_llm = llm.with_structured_output(AgentResponse)
-react_prompt_with_format_instructions = PromptTemplate(
-    input_variables=["input", "agent_scratchpad", "tool_names"],
-    template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
-).partial(format_instructions=output_parser.get_format_instructions())
+tools = [TavilySearch()]
+llm = ChatOllama(temperature=0, model="qwen2.5:7b")
 
-agent = create_react_agent(
-    llm=llm,
+
+agent = create_agent(
+    model=llm,
     tools=tools,
-    prompt=react_prompt_with_format_instructions,
+    response_format=AgentResponse
 )
-agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
-extract_output = RunnableLambda(lambda x: x["output"])
-parse_ouput = RunnableLambda(lambda x: output_parser.parse(x))
-chain = agent_executor | extract_output | parse_ouput
 
 def main():
-    result = chain.invoke(
-        input = {
-            "input": "search for 3 job summer internship postings for a data-related position in singapore on linkedin and list their details"
+    # human_msg = HumanMessage("search for 3 job summer internship postings for a data-related position in singapore on linkedin and list their details")
+    # messages = [human_msg]
+    result = agent.invoke(
+        {
+            "messages": [
+                {
+                    "role": "user",
+                    "content": "search for 3 job summer internship postings for a data-related position in singapore on linkedin and list their details",
+                }
+            ]
         }
     )
-    # Access structured response from the agent
-    # structured = result.get("structured_response", None)
-    # print(structured)
     print(result)
 
 
